13d5c30b33022aa526e329c6677e57dfbcf4e306
Nate Brennand <natebrennand@gmail.com>
2014-05-26 22:54:03 -0700

    init

---
+++ config/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+
+sudo apt-get install -y golang
+
+sudo apt-get install -y xvfb
+sudo apt-get install -y avconv
+sudo apt-get install -y libav-tools
+sudo apt-get install -y alsa-utils
+sudo apt-get install -y xdotool
+
+
