d2b1bbe74cae08a1eb1dbe5a46cb5825e96a3e5b
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-09-16 18:17:53 -0400

    usernames work

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,10 @@ $(function() {
     $('.key').click(function(e) {
         e.preventDefault();
         var key = $(this).data('key');
-        $.post('/press', JSON.stringify({'key': key}));
+        var username = $('.name').val();
+        $.post('/press', JSON.stringify({
+            'key': key,
+            'username': username
+        }));
     });
 });
