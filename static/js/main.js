93d6ad4c37a95f9b10aeda0984516baf3b98b7d5
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-09-16 15:10:52 -0400

    brown working buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
-
+$(function() {
+    $('.key').click(function(e) {
+        e.preventDefault();
+        var key = $(this).data('key');
+        $.post('/press', {
+            'key': key
+        });
+    });
+});
